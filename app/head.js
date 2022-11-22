eb648a87865ff237d4b697e6a11e64316544ee6a
Aman Juman <alamanshogun@gmail.com>
2022-11-23 04:48:59 +0600

    first commit

--- app/head.js
+++ app/head.js
@@ -3,6 +3,8 @@ export default function Head() {
         <>
             <title>Сарыагаш Санатории</title>
             <meta name="description" content="Сарыагаш санатории"/>
+            <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="icon" href="/favicon.ico"/>
         </>
     )
